277768a993d70422676937b524246d83bc98a432
pritam-mb <patrapritam567@gmail.com>
2025-09-04 00:49:24 +0530

    ts install

---
+++ purets/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("heelo world");
+console.log("fuck u");
+console.log("lolo");
+console.log("456");
+console.log("dmemk");
+console.log("lkol");
+console.log("lopo");
+//# sourceMappingURL=index.js.map
---
+++ purets/src/index.ts
@@ -0,0 +1,9 @@
+console.log("heelo world");
+console.log("fuck u");
+console.log("lolo");
+console.log("456");
+console.log("dmemk");
+console.log("lkol");
+
+
+console.log("lopo");
